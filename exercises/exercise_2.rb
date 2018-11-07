055062acca5b30f640a99aa015825abdb76ca85d
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-06 21:34:11 -0800

    assigned store 1 and 2 to variables & updated store 3 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.name = 'Hamptons'
+
+@store1.save
+
+
 # Your code goes here ...
