60b25859e936d4e13e2c62cde254f364fb2bcdfb
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-06 20:57:36 -0800

    create 3 stores in database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+
+Store.create(location: 'Burnaby', annual_revenue: 300000, womens_apparel: true, mens_apparel: true)
+Store.create(location: 'Richmond', annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
+Store.create(location: 'Gastown', annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
+
+puts Store.count
 # Your code goes below here ...
