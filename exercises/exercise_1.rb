4c007c0864d2a4d705171d5a84a2c1118e7a6272
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-06 21:14:49 -0800

    change location key to name as per psql

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 
-Store.create(location: 'Burnaby', annual_revenue: 300000, womens_apparel: true, mens_apparel: true)
-Store.create(location: 'Richmond', annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
-Store.create(location: 'Gastown', annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
+Store.create(name: 'Burnaby', annual_revenue: 300000, womens_apparel: true, mens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
+Store.create(name: 'Gastown', annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
 
 puts Store.count
 # Your code goes below here ...
