bb4dfdc22b60fb8371937ff526121537205956d2
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-06 22:39:08 -0800

    create 3 new stores, assign men and womens stores to variables

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,19 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
+Store.create(name: 'Surrey', annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+Store.create(name: 'Whistler', annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
+Store.create(name: 'Yaletown', annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+@mens_store = Store.where(mens_apparel: true)
+@mens_store.all.each do |s|
+  puts "name: #{s.name} annual revenue: #{s.annual_revenue}"
+end
+
+@womens_store = Store.where('womens_apparel = ? and annual_revenue < ?', true, 1000000)
+
+
+
+
+
 # Your code goes here ...
