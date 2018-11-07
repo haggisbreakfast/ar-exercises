8906ee5040a7088a9d601f7157c8df9c591c982f
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-06 22:54:10 -0800

    calculate av annual revenue of stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,9 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+total_revenue = Store.sum('annual_revenue')
+
+puts total_revenue/Store.count
+puts Store.where('annual_revenue > ?', 1000000).count
+
 # Your code goes here ...
